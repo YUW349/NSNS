65e9dc428500ef72031c091928a5258ff1dd5031
Kai-Chin Shih <k3shih@ucsd.edu>
2018-02-22 14:39:28 -0800

    change major

--- routes/input.js
+++ routes/input.js
@@ -6,14 +6,29 @@ exports.addNewTask = function(req,res){
 	var time3 = req.query.time3;
 	var location = req.query.location;
 	var count = Object.keys(data.tasks).length ;
-	
-	var newTask = { 
-              "taskname" : "Task Name: " + taskname,
-              "time" : "Time: " + time+time2+time3,
-              "location" : "Location: " + location,
-              "id" : count
 
-	}
+	var countDownDate = new Date("Feb 22, 2018 " + time + ":" + time2 + ":" + time3).getTime();
+		var now = new Date().getTime();
+		var distance = countDownDate - now;
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+		if(distance<0){
+        		var countdown = "overdue";
+				}else{
+				var countdown = "Countdown: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+				}
+		
+	var newTask = { 
+	            "taskname" : "Task Name: " + taskname,
+				"time" : "Time: " + time+time2+time3,
+				"countdown" : countdown,
+				"location" : "Location: " + location,
+				"id" : count
+		}
+
     console.log(count);
 	//console.log(newTask);
     data.tasks.push(newTask);
