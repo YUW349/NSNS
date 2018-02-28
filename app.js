026875caed3e1150df68cd25e4de030439cf816f
Yujie Wang <Yuw349@ucsd.edu>
2018-02-27 19:20:01 -0800

    ....

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', index.view);
-app.get('/viewAlt',home.viewAlt);
+app.get('/home.html/viewAlt',home.viewAlt);
 app.get('/home', index.viewHome);
 app.get('/home', home.login); 
 app.get('/home',home.view);
